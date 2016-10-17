9a18dfc161cae920acb0929b8c177d2d4e4d362e
alex@digisavvy.com <alex@digisavvy.com>
2016-10-17 15:46:07 -0700

    modify copyblock

--- dg-copyblock/includes/frontend.php
+++ dg-copyblock/includes/frontend.php
@@ -1,24 +1,22 @@
 <?php
 $background = $settings->dg_field_side_image;
-$bgimg = 'background: url();';
+
+$image_id = $background;
+$image_url = wp_get_attachment_image_src($background,'full');
+$image_url = $image_url[0];
+
+$bgimg = 'background: url('.$image_url.');';
 $bgsize = 'background-size: cover;';
 $repeat = 'background-repeat: no-repeat;';
 $border_bottom = 'border-bottom: 1px solid rgba(38, 74, 38, 0.2);';
 $display = 'display: block;';
 $minh = 'min-height: 12em;';
 $padding = "padding: 3em;";
- 
+
 ?>
-
-<?php // var_dump($background); ?>
+<?php // echo  $image_url; ?>
 <div class="side-image">
-<figure>
-	<?php echo $background; ?>
-	<?php var_dump($display); ?>
-	<?php // echo $src; ?>
-</figure>
-
-	<div class="images-wrapper" style="<?php echo $bgimg, $bgsize, $repeat, $border_bottom, $display, $minh, $padding;  ?>"></div>
+	<div class="images-wrapper" style="<?php echo $bgimg, $bgsize, $repeat, $border_bottom, $minh, $padding;  ?>"></div>
 	<div class="side-image-content">
 		<h4><?php echo $settings->dg_field_headline; ?></h4>
 		<?php echo $settings->dg_field_copy; ?>
