1559ed3b02f663ab4695da6b809637f66a6fd063
alex@digisavvy.com <alex@digisavvy.com>
2016-10-31 17:18:21 -0700

    add parallax module settings

--- dg-parallax/dg-parallax.php
+++ dg-parallax/dg-parallax.php
@@ -25,6 +25,39 @@ FLBuilder::register_module( 'DGParallaxModuleClass', array(
 			'my-section-1'  => array(
 				'title'         => __( 'Section 1', 'fl-builder' ),
 				'fields'        => array(
+                    'dg_field_parallax_heading' => array(
+                        'type'          => 'text',
+                        'label'         => __('Heading', 'fl-builder'),
+                        'description'   => 'Copy that overlays your parallax image'
+                    ),
+                    'dg_field_font_size'     => array(
+                        'type'          => 'select',
+                        'label'         => __('Font Size', 'fl-builder'),
+                        'default'       => 'default',
+                        'options'       => array(
+                            'default'       =>  __('Default', 'fl-builder'),
+                            'custom'        =>  __('Custom', 'fl-builder')
+                        ),
+                        'toggle'        => array(
+                            'custom'        => array(
+                                'fields'        => array('dg_field_custom_font_size')
+                            )
+                        )
+                    ),
+                    'dg_field_custom_font_size' => array(
+                        'type'          => 'text',
+                        'label'         => __('Custom Font Size', 'fl-builder'),
+                        'default'       => '24',
+                        'maxlength'     => '3',
+                        'size'          => '4',
+                        'description'   => 'px'
+                    ),
+                    'dg_field_parallax_text_color' => array(
+                        'type'          => 'color',
+                        'label'         => __( 'Text Overlay Color', 'fl-builder' ),
+                        'default'       => '333333',
+                        'show_reset'    => true
+                    ),
 					'dg_field_parallax_image' => array(
 						'type'          => 'photo',
 						'label'         => __('Parallax Image', 'fl-builder'),
